99ace49f0f24904cfa28f32b036996e76532f90b
ducdaudshp <ducdaudshp@gmail.com>
2022-02-07 16:23:22 +0700

    pt bac nhat

---
+++ ptbacnhat/src/ptbacnhat.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ptbacnhat {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("a= ");
+        double a=scanner.nextDouble();
+        System.out.print("b= ");
+        double b= scanner.nextDouble();
+        System.out.print("c= ");
+        double c= scanner.nextDouble();
+        if (a!=0){
+            double answer=(c-b)/a;
+            System.out.printf("Kết quả x=%f\n",answer);
+        }
+        else if (b==c){
+            System.out.print("x = 0");
+        }
+        else{
+            System.out.println("Vô nghiệm");
+        }
+    }
+}
